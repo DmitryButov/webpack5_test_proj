31bac3a7be31424544ec6cc0fc435552f4125f26
Dmitrii Butov <bdswork@mail.ru>
2021-10-06 21:54:43 +0400

    [wp] use 2 bundles (2 entries)

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,7 +2,10 @@ const path = require('path')
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
-    entry: './index.js',
+    entry: {
+      main: './index.js',
+      analytics: './analytics.js'
+    },
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
